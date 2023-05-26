16a4505ef6334d1acc74fe777e5d62ea739fa7f9
Ben Morris <benmor2020@gmail.com>
2023-05-26 16:16:27 -0400

    First past at hello world

--- src/client.c
+++ src/client.c
@@ -1,8 +1,38 @@
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "utils.h"
 #include "client_funcs.h"
 #include "constants.h"
+
+int hello_world_client(FILE* socket_file) {
+    char* recv_line = NULL;
+    size_t recv_line_size = 0;
+    puts("Waiting for server...");
+    if (getline(&recv_line, &recv_line_size, socket_file) == -1) {
+        return -1;
+    }
+    puts("Received line");
+    printf("%s", recv_line);
+    free(recv_line);
+
+    char* send_line = NULL;
+    size_t send_line_size = 0;
+    if (getline(&send_line, &send_line_size, stdin) == -1) {
+        return -1;
+    }
+    puts("Got input");
+    // If we can't send the line on the socket, the connection is broken and we
+    // have to exit.
+    if (fputs(send_line, socket_file) == EOF) {
+        free(send_line);
+        error_and_exit("Couldn't send line");
+    }
+    puts("Sent input");
+    free(send_line);
+    return 0;
+}
 
 // Code modified from olincollege/softsys A8
 int main(void) {
@@ -11,12 +41,13 @@ int main(void) {
     struct sockaddr_in server_addr = socket_address(INADDR_LOOPBACK,
             LISTEN_PORT);
     try_connect(socket, server_addr);
+    puts("Connected");
 
     // Run client
     FILE* socket_file = get_socket_file(socket);
     int socket_file_status = 0;
     while (socket_file_status != -1) {
-        socket_file_status = -1; // TODO replace with actual client function
+        socket_file_status = hello_world_client(socket_file);
     }
 
     // If we didn't hit the end of the file, something went wrong
--- src/server.c
+++ src/server.c
@@ -1,10 +1,31 @@
 
-
+#include <stdio.h>
+#include <string.h>
 #include "server_funcs.h"
 #include "constants.h"
 #include "utils.h"
 
 void hello_world_server(int client_socket) {
+    FILE* client_file = fdopen(client_socket, "r+");
+    if (client_file == NULL) {
+        error_and_exit("Error opening client_file file");
+    }
+    char* line = NULL;
+    size_t len = 0;
+    while (1) {
+        if (fprintf(client_file, "Hello, ") <= 0) {
+            error_and_exit("Error writing to client_file");
+        }
+        puts("Sent text");
+        if (getline(&line, &len, client_file) == -1) {
+            error_and_exit("Error reading line");
+        }
+        printf("Received %s\n", line);
+        if (strcmp(line, "World!") == 0) {
+            return;
+        }
+        puts("Line unaccepted, continuing");
+    }
 }
 
 // Code modified from olincollege/softsys A8
@@ -16,8 +37,10 @@ int main(void) {
 
     // Setup listener and accept clients
     listen_for_connections(server);
-    while (accept_client(server) != -1) {
-        accept_client(server);
+    int accept_status = 0;
+    while (accept_status != -1) {
+        accept_status = accept_client(server);
+        puts("Accepted client");
     }
 
     // Clean up and return
